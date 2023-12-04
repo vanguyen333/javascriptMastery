7e71f24660359a2197ad8cc91af68405a21ed6ff
vanguyen333 <vanguyen333@yahoo.com>
2023-12-03 22:54:14 -0500

    null, undefined, objects

---
+++ Variables & Data Types/Null and Undefined/script.js
@@ -0,0 +1,11 @@
+// null: has only 1 value null means: emptly, value unknown
+const age = null;
+console.log(age); //object , an error/bug in javascript
+age = 20;
+
+console.log(age);
+
+// undefined: valued is not assigned, variable is declared but not assigned a value
+// undfined is a default value assigned by javascript is a variable is not assigned a value
+let x; //no value is not assigned =>undefined dataype/value
+console.log(x); //undefined
---
+++ Variables & Data Types/Objects/script.js
@@ -0,0 +1,12 @@
+//Objects : store collection of data
+
+//person is an entity/object, has key: name and value of name is John
+const person = {
+  name: "John",
+  age: 25,
+};
+console.log(person.name);
+console.log(typeof person); //object
+
+const date = new Date(); //an object
+console.log(date);
