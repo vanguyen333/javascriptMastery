88ac96d8537aa398448a899f0c4cbde9417854d8
vanguyen333 <vanguyen333@yahoo.com>
2023-12-05 16:24:19 -0500

    6 falsy values false,0,empty string, undfined, not a number

---
+++ Control flow/script.js
@@ -0,0 +1,15 @@
+//6 Falsy values ==> false, everything else is truful values:
+// false;
+// 0;
+// ("");
+// null;
+// undefined;
+// NaN;
+
+//Trufull values, for example
+// 1;
+// ("hello");
+// 58;
+// {
+// }
+// [];
