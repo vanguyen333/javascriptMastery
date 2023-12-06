b601e4cb9b592ce7ce84c757f7968e344f7d3c73
vanguyen333 <vanguyen333@yahoo.com>
2023-12-06 10:05:07 -0500

    funtion types

---
+++ functions/script.js
@@ -0,0 +1,19 @@
+function square(number) {
+  return number * number;
+}
+const result = square(5);
+
+console.log(result);
+//A Function Declaration
+function name(parameters) {
+  statements;
+}
+//A Function Expressions
+let name = function (parameters) {
+  statements;
+};
+
+// Arrow Function
+const name = (parameters) => {
+  statements;
+};
