99a2a6fc20dab4c690e4741fd91d30852b63ef55
vanguyen333 <vanguyen333@yahoo.com>
2023-12-06 10:19:41 -0500

    parameters vs arguments

--- functions/script.js
+++ functions/script.js
@@ -17,3 +17,22 @@ let name = function (parameters) {
 const name = (parameters) => {
   statements;
 };
+//for 2 return keywords, only the first return one is executed
+function test() {
+  return true;
+  return false;
+}
+
+test(); // true
+// Parameters are used when defining a function,
+// they are the names created in the function definition.
+// Parameter is like a variable that is only meaningful inside of this function. It won't be accessible outside of the function.
+
+// Arguments are real values passed to the function
+// when making a function call.
+
+const sayHi = (firstName) => {
+  console.log(`Hi, ${firstName}`);
+};
+
+sayHi("Joe"); // Hi Joe
