4b1a4acac1d2de566fffc33431594d5f772fc232
vanguyen333 <vanguyen333@yahoo.com>
2023-12-07 11:54:44 -0500

    array intro

---
+++ Arrays/script.js
@@ -0,0 +1,13 @@
+const arr = [
+  "Apple",
+  { name: "John" },
+  true,
+  function () {
+    alert("hello");
+  },
+];
+
+const months = ["January", "February", "March", "April"];
+for (let i = 0; i < months.length; i++) {
+  console.log(months[i]);
+}
