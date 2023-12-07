2c3e45999fe81887a0448dfe7c7be2a1c0c62bc1
vanguyen333 <vanguyen333@yahoo.com>
2023-12-07 12:42:17 -0500

    forEach method

---
+++ Arrays/array for each/script.js
@@ -0,0 +1,9 @@
+// const names = ["oanh", "ngoc", "lan", "hoa"];
+// names.forEach((name, index) => {
+//   console.log(name, index);
+// });
+
+let sum = 0;
+const numbers = [1, 2, 3, 4, 5, 6];
+numbers.forEach((number, i) => (sum += number));
+console.log(sum);
