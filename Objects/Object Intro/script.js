6b11679cc77a10c876af5f372f8ee5db3b059aee
vanguyen333 <vanguyen333@yahoo.com>
2023-12-09 09:41:12 -0500

    object intro

---
+++ Objects/Object Intro/script.js
@@ -0,0 +1,22 @@
+// unordered collection of related data with key and value
+
+const person = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 25,
+  car: {
+    year: 2015,
+    color: "Red",
+  },
+};
+
+const firstName = "Johnny";
+
+const person1 = {
+  firstName: firstName,
+};
+//or shorter form
+
+// const person1 = {
+// 	firstName,
+// }
