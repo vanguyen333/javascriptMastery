597c6597fc9b4e0147c228f4845d8a373ceaeabb
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 17:01:59 -0500

    cloning objects

--- Value vs Reference/Shallow Cloning/script.js
+++ Value vs Reference/Shallow Cloning/script.js
@@ -19,3 +19,18 @@ console.log(newNumbers); //[1, 2, 3, 4, 5]
 // 2nd way array.slice(): get different location in memory
 const newNums = numbers.slice();
 console.log(newNums); //[1, 2, 3, 4, 5, 6]
+
+// CLONING OBJECTS
+// 1st way: spread operator ...
+
+const pet = { name: "bong bong", age: 3 };
+const otherPet = { ...pet };
+pet.age = 2;
+console.log(pet, otherPet); // {name: 'bong bong', age: 3}
+
+// 2nd way: Object.assign()
+const flower = { name: "Rose", age: 1 };
+const flower1 = Object.assign({}, flower);
+flower.age = 3;
+console.log(flower); //{name: 'Rose', age: 3}
+console.log(flower1); // {name: 'Rose', age: 1}
