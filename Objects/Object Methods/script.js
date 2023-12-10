8d0966129b0629c6b273cebe227df3a2ba8162c9
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 15:30:19 -0500

    Object.entries() return key and value

--- Objects/Object Methods/script.js
+++ Objects/Object Methods/script.js
@@ -21,3 +21,20 @@ const session = {
 };
 const sessionInfo = Object.values(session);
 console.log(sessionInfo);
+
+// Object.entries() creates a nested array of the key/value pairs of an object
+
+const operatingSystem = {
+  name: "Ubuntu",
+  version: 18.04,
+  license: "Open Source",
+};
+
+const entries = Object.entries(operatingSystem);
+console.log(entries);
+// Loop through the results:
+entries.forEach((entry) => {
+  let key = entry[0];
+  let value = entry[1];
+  console.log(`${key}: ${value}`);
+});
