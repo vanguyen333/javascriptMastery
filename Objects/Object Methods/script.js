ba30ce2219879ec5609c57d319bbab0bee31e8bd
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 15:23:10 -0500

    Object.keys()

---
+++ Objects/Object Methods/script.js
@@ -0,0 +1,13 @@
+// Object.keys() creates an array containing the keys of an object.
+
+// Initialize an object
+
+const employees = {
+  boss: "Michael",
+  secretary: "Pam",
+  sales: "Jim",
+  accountant: "Oscar",
+};
+
+const position = Object.keys(employees);
+console.log(position);
