c3eecbcf5cfdd328d799658ea11ca72821bd17ad
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 15:26:00 -0500

    Object.values()

--- Objects/Object Methods/script.js
+++ Objects/Object Methods/script.js
@@ -11,3 +11,13 @@ const employees = {
 
 const position = Object.keys(employees);
 console.log(position);
+
+// Object.values() cerates an array containing the values of an object
+const session = {
+  id: 1,
+  time: `26-July-2018`,
+  device: "mobile",
+  browser: "Chrome",
+};
+const sessionInfo = Object.values(session);
+console.log(sessionInfo);
