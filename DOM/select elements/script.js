4527f0dfb95a372910509098747516249d1bdeac
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 21:53:41 -0500

    elements properties and methods

--- DOM/select elements/script.js
+++ DOM/select elements/script.js
@@ -1,4 +1,21 @@
 const el = document.getElementById("element-below-input");
-const element = document.getElementsByClassName("test");
+const sb = document.getElementsByClassName("test");
 const elm = document.querySelectorAll(".test"); //all elements with class of 'test'
 //and so on...
+
+//Elements Properties and Methods
+var element = document.getElementById("div1");
+
+console.log(element.classlist); // ["div1", "div2", value: "div1 div2"]
+
+console.log(element.className); // "div1 div2"
+
+console.log(element.id); // "div1"
+
+console.log(element.innerHtml); // "<span>hello</span>"
+
+const button = document.querySelector("button");
+button.addEventListener("click", () => {
+  alert("You clicked on the button");
+});
+button.hasAttribute("type"); //true
