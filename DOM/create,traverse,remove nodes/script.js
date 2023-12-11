5d74ce51e43c82a59028bd66c3a08077d4d02e78
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 22:27:46 -0500

    create an element to the DOM

---
+++ DOM/create,traverse,remove nodes/script.js
@@ -0,0 +1,5 @@
+document.createElement("h1");
+const heading = document.createElement("h1");
+const body = document.querySelector("body");
+body.appendChild(heading);
+heading.innerText = "Hello World";
