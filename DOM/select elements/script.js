35ad1e2e28ccc88b23de8a2ac3fe3c159a666403
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 21:46:19 -0500

    DOM select elements

---
+++ DOM/select elements/script.js
@@ -0,0 +1,4 @@
+const el = document.getElementById("element-below-input");
+const element = document.getElementsByClassName("test");
+const elm = document.querySelectorAll(".test"); //all elements with class of 'test'
+//and so on...
