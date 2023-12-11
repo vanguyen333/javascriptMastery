43aa09acfcc43f222735fe1d89f458c4babea7db
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 22:05:56 -0500

    working with classes

---
+++ DOM/working with classes/script.js
@@ -0,0 +1,7 @@
+const menuClick = (currEl) => {
+  const menuItems = document.getElementsByClassName("menu-item");
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].classList.remove("active");
+  }
+  currEl.classList.add("active");
+};
