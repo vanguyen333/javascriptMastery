97a32fc1cbc1a7eae743ca866765426d8b4c351a
vanguyen333 <vanguyen333@yahoo.com>
2023-12-10 22:32:08 -0500

    traverse nodes

--- DOM/create,traverse,remove nodes/script.js
+++ DOM/create,traverse,remove nodes/script.js
@@ -3,3 +3,14 @@ const heading = document.createElement("h1");
 const body = document.querySelector("body");
 body.appendChild(heading);
 heading.innerText = "Hello World";
+
+//Traverse
+const subjects = document.querySelector(".subjects");
+subjects.firstElementChild;
+subjects.lastElementChild;
+
+const favSubject = document.querySelector(".fav-subject");
+favSubject;
+favSubject.previousElementSibling;
+favSubject.nextElementSibling;
+favSubject.parentElement;
