b0151676ad3b77be40a44a58bdb0884dd93e653f
vanguyen333 <vanguyen333@yahoo.com>
2023-12-11 11:06:07 -0500

    classes, the new key word

---
+++ Classes, new and this key words/New key word/script.js
@@ -0,0 +1,20 @@
+// The new key word creates a new object or array
+const person = {};
+const person1 = new Object();
+person.firstName = "John";
+person1.firstName = "John";
+
+console.log(typeof person);
+console.log(typeof person1);
+
+const arr = [1, 2, 3];
+const arr1 = new Array(1, 2, 3);
+
+const myDate = new Date("Dec 11, 2023");
+console.log(myDate);
+console.log(myDate.getFullYear());
+
+console.log(Array);
+console.log(Array);
+console.log(Number);
+console.log(Date);
