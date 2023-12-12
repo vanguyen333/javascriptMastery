ec175bacd4e2f7bc97318a752e81d181932c64a6
vanguyen333 <vanguyen333@yahoo.com>
2023-12-12 17:00:56 -0500

    Asynchronous JavaScript

--- Asynchronous javaScript/asynchronous intro/script.js
+++ Asynchronous javaScript/asynchronous intro/script.js
@@ -22,3 +22,31 @@ functionOne();
 // Function One
 // Function Two
 // Function One, Part 2
+
+// Asynchronous JavaScript is a one in which some lines of code
+// take time to run.
+// These tasks are run in the background
+// while the JavaScript engine keeps executing other lines of code
+// When the result of the asynchronous tasks gets available,
+// is then used in the program.
+
+const functionMot = () => {
+  console.log("Function Mot"); // 1
+
+  functionHai();
+
+  console.log("Function One, Part hai"); // 2
+};
+
+const functionHai = () => {
+  setTimeout(() => {
+    console.log("Function hai");
+  }, 3000); // 3
+};
+
+functionOne();
+
+// Function Mot
+// Function One, Part hai
+//(after 3 seconds delay)
+// Function hai
