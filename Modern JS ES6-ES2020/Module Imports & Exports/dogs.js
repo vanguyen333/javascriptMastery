e4069a9b4dffc893f4448dea257451b3323489e2
vanguyen333 <vanguyen333@yahoo.com>
2023-12-12 19:50:47 -0500

    imports and exports

---
+++ Modern JS ES6-ES2020/Module Imports & Exports/dogs.js
@@ -0,0 +1,3 @@
+export const dogs = ["Bear", "Fluffy", "Doggo"];
+
+export const numberOfDogs = dogs.length;
---
+++ Modern JS ES6-ES2020/Module Imports & Exports/script.js
@@ -0,0 +1,8 @@
+// IMPORTS AND EXPORTS
+import { dogs, numberOfDogs } from "./dogs";
+import onlyOneThing from "./test";
+
+console.log(`I have ${numberOfDogs} dogs`);
+console.log(`<y dogs are: ...${dogs}`);
+
+console.log(onlyOneThing);
